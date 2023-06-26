f80d9449a0faebe1a0102a878eb295ae41a0c8e1
v-nebe <katya.shauneva@gmail.com>
2023-06-26 13:01:46 +0300

    tasks completed

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -21,11 +21,17 @@ public class ParseIntegers {
         String justWords = "";
         while (words.hasNext()) {
             String next = words.next();
-            int number = Integer.parseInt(next);
-            // todo: complete it
+            try {
+                int number = Integer.parseInt(next);
+                sum += number;
+                // todo: complete it
+            }catch (NumberFormatException e){
+                justWords += next + " ";
+            }
+
         }
         System.out.println("Sum is " + sum);
-        System.out.println("Just words:" + justWords);
+        System.out.println("Just words: " + justWords);
     }
 }
 
